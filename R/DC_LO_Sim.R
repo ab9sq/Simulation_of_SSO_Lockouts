515c435bb2da069dfbd87b54bdf47e591f89db39
Lauerman <nick.lauerman@abbott.com>
2021-06-22 08:35:10 -0500

    randome site generator cre4ated

--- R/DC_LO_Sim.R
+++ R/DC_LO_Sim.R
@@ -7,3 +7,19 @@
 
 
 # rnd function/call
+site <- function(sites = 2) {
+     # error checks
+     if (is.null(sites)){
+          sites <- 2
+     }
+     if ( ( (sites < 2)) | (is.na(sites))) {
+          sites <- 2
+     }
+     if (!is.numeric(sites)){
+          sites <- 2
+     }
+     # functions work
+     sites <- as.integer(sites)
+     roll <- as.integer(stats::runif(1, min = 1, max = sites + 1))
+     return(roll)
+}
