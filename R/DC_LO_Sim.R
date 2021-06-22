ab813daf474e76088ef89de4f7b50c7969729fc3
Lauerman <nick.lauerman@abbott.com>
2021-06-22 10:43:43 -0500

    V1 of functions

--- R/DC_LO_Sim.R
+++ R/DC_LO_Sim.R
@@ -1,14 +1,29 @@
 # simulation run function
-
-
-
-
-
-
+sim <- function(run = 100,
+                sites = 2){
+     runs <- 1:run
+     dcCount[1:sites] <- 0
+     test <- FALSE
+     trialCount <- 0
+     results <- NULL
+     for(index in runs){
+          while(test == FALSE){
+               trialCount <- trialCount + 1
+               dataCenter <- siteSelectioned(sites = sites)
+               dcCount[dataCenter] <- dcCount[dataCenter] + 1
+               if(dcCount[dataCenter] >= 10){
+                    test <- TRUE
+               }
+          }
+          results[index] <- trialCount
+          trialCount <- 0
+     }
+     return(results)
+}
 
 # rnd function/call
-site <- function(sites = 2) {
-     # error checks
+siteSelectioned <- function(sites = 2) {
+     # error checks and corrections
      if (is.null(sites)){
           sites <- 2
      }
@@ -20,6 +35,6 @@ site <- function(sites = 2) {
      }
      # functions work
      sites <- as.integer(sites)
-     roll <- as.integer(stats::runif(1, min = 1, max = sites + 1))
-     return(roll)
+     sites <- as.integer(stats::runif(1, min = 1, max = sites + 1))
+     return(sites)
 }
